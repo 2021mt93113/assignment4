82d4dbec76d155a339ea131481fafcb9b88beb35
skbolisetti <2021mt93153@wilp.bits-pilani.ac.in>
2022-05-05 20:05:50 +0530

    censor_string to return string instead of printing

--- 1.CensorWords/Driver.py
+++ 1.CensorWords/Driver.py
@@ -1,7 +1,6 @@
 from CensorWords import censor_string
 
 if __name__ == '__main__':
-    censor_string("Today is a Wednesday!", ["Today", "a"], "-")
-    censor_string("The cow jumped over the moon.", ["cow", "over"], "*")
-    censor_string("Why did the chicken cross the road?", ["Did", "chicken", "road"], "*")
-
+    print(censor_string("Today is a Wednesday!", ["Today", "a"], "-"))
+    print(censor_string("The cow jumped over the moon.", ["cow", "over"], "*"))
+    print(censor_string("Why did the chicken cross the road?", ["Did", "chicken", "road"], "*"))
