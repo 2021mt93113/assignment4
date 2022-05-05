c1c9af9c67b76f1e6fcb2ca308c2fb1b571e2e70
skbolisetti <2021mt93153@wilp.bits-pilani.ac.in>
2022-05-05 19:37:49 +0530

    Extract to module

--- 1.CensorWords/1.CensorWords.py
+++
@@ -1,9 +0,0 @@
-def censor_string(string, lst, char):
- modified_string=string.split()
- returned_words = [word if word not in lst else char*len(word) for word in modified_string]
- print (' '.join(returned_words))
-
-
-censor_string("Today is a Wednesday!", ["Today", "a"], "-")
-censor_string("The cow jumped over the moon.", ["cow", "over"], "*")
-censor_string("Why did the chicken cross the road?", ["Did", "chicken", "road"], "*")
